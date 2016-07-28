5fff2f2279f880fd9ddc3174fab2b4c1fecd0ed9
nhunght <nhunghoang1609@gmail.com>
2016-07-28 16:23:25 +0700

    generate scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160728085641) do
+ActiveRecord::Schema.define(version: 20160728091324) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "content"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(name: "Hoang Thi Nhung",
+  email: "hoang.thi.nhung@framgia.com",
+  password: "12345678")
+
+User.create(name: "Toshiaki",
+  email: "toshiaki@gmail.com",
+  password: "12345678")
